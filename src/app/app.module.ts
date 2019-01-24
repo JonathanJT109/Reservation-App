c5f0591c53a649e90bcf33cdc7ae4f162ffeb558
JonathanJT109 <44759217+JonathanJT109@users.noreply.github.com>
2019-01-23 19:15:23 -0600

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material';
+import { FormComponent } from './form/form.component';
+import { HeaderComponent } from './header/header.component';
+import { EditMessageComponent } from './edit-message/edit-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HeaderComponent,
+    EditMessageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
